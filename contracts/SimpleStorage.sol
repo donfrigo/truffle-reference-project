a90264a7c948178a1ff9f86c9f908bd433f4a31c
Jani <donfrigo@gmail.com>
2020-03-20 13:14:56 +0100

    Update SimpleStorage.sol

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,12 +2,28 @@ pragma solidity ^0.5.0;
 
 contract SimpleStorage {
   uint storedData;
+  uint storedData2;
 
-  function set(uint x) public {
+  function set(uint x, uint y) public {
     storedData = x;
+    storedData2 = y;
   }
 
-  function get() public view returns (uint) {
+  function get1() public view returns (uint) {
     return storedData;
+  }
+  
+   function get2() public view returns (uint) {
+    return storedData2;
+  }
+  
+  
+  function getContractValue() public view returns (uint) {
+    return address(this).balance;
+  }
+  
+  
+  function payMFO() external payable returns (uint) {
+    return msg.value;
   }
 }
